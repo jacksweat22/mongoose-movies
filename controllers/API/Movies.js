07ff2c3444ec6f9483d66d85af475998833b0f4c
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-05-27 14:50:59 -0700

    create and update

--- controllers/API/Movies.js
+++ controllers/API/Movies.js
@@ -8,12 +8,25 @@ const index = (req, res) => {
 
 const show =(req, res) =>{
     Movie.findById(req.params.id).populate('cast').then((movie) =>{ res.status(404).json(movie)
+    })
+}
 
+const create = (req, res) => {
+    Movie.create(req.body).then((movie) =>{
+        res.status(202).json(movie)
+    })
+}
+
+const update =(req,res) => {
+    Movie.findByIdAndRemove(req.params.id).then((movie)=> {
+        res.status(200).json(movie)
     })
 }
 
 module.exports = {
     index,
     show,
-    
+    create,
+    update,
+
 }
